34e90b38cc9a83d96a7d989273a1d963eaeb9814
AkshayKumar007 <akshaykrcc@live.com>
2019-10-26 20:33:09 +0530

    some code to start with

--- People_ify/People_ify/settings.py
+++ People_ify/People_ify/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'People_ify',
         'USER': 'postgres',
-        'PASSWORD': 'akshay',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
