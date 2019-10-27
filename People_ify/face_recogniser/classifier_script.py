b31a5dba63e5a9902e58235f3a0c77cb2a3b4bb4
AkshayKumar007 <akshaykrcc@live.com>
2019-10-27 21:32:46 +0530

    face

--- People_ify/face_recogniser/classifier_script.py
+++ People_ify/face_recogniser/classifier_script.py
@@ -1,11 +1,11 @@
 '''
-script to upload images to azure-cognitiveservices-api
+code to upload images to azure-cognitiveservices-api and group them
 '''
 
 import asyncio, io, glob, os, sys, time, uuid, requests
 from urllib.parse import urlparse
 from io import BytesIO
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw #  Pillow
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
@@ -18,3 +18,29 @@ KEY = os.environ['FACE_SUBSCRIPTION_KEY']
 ENDPOINT = os.environ['FACE_ENDPOINT']
 # Create an authenticated FaceClient.
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+def main():
+    # Detect a face in an image that contains a single face
+    # basically code to check if there is a face, if not  
+    single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
+    single_image_name = os.path.basename(single_face_image_url)
+    detected_faces = face_client.face.detect_with_url(url=single_face_image_url)  #  extracing name of file from url
+    if not detected_faces:
+        raise Exception('No face detected from image {}'.format(single_image_name))
+        #  add code to sort in 'objects/ no human' folder
+
+
+    # Display the detected 'face ID' in the first single-face image.
+    # Face IDs are used for comparison to faces (their IDs) detected in other images.
+    print('Detected face ID from', single_image_name, ':')
+    for face in detected_faces: 
+        print (face.face_id)
+    print()
+    # Save this ID for use in Find Similar
+    first_image_face_ID = detected_faces[0].face_id
+
+
+
+
+if "__name__" == "__main__":
+    main()
