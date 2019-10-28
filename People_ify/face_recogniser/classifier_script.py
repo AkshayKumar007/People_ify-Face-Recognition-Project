2c1e6ea804a3a216a43919756db685ac4f662a76
AkshayKumar007 <akshaykrcc@live.com>
2019-10-28 15:19:43 +0530

    improvements in face

--- People_ify/face_recogniser/classifier_script.py
+++ People_ify/face_recogniser/classifier_script.py
@@ -5,26 +5,59 @@ code to upload images to azure-cognitiveservices-api and group them
 import asyncio, io, glob, os, sys, time, uuid, requests
 from urllib.parse import urlparse
 from io import BytesIO
-from PIL import Image, ImageDraw #  Pillow
+from PIL import Image, ImageDraw  # Pillow
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
 # Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
-# This key will serve all examples in this document.
 KEY = os.environ['FACE_SUBSCRIPTION_KEY']
-# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.
-# This endpoint will be used in all examples in this quickstart.
+# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.(in our case Central India)
 ENDPOINT = os.environ['FACE_ENDPOINT']
 # Create an authenticated FaceClient.
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+
+def identify_face(image, face_ids, PERSON_GROUP_ID):
+    # Identify faces
+    results = face_client.face.identify(face_ids, PERSON_GROUP_ID)
+    print('Identifying faces in {}')
+    if not results:
+        # Define new persons
+        for x in face_ids:
+            person = face_client.person_group_person.create(PERSON_GROUP_ID, x)
+            # create a seperate folder for this person
+            # print('No person identified in the person group for faces from the{}.'.format(os.path.basename(image.name)))
+    else:
+        for person in results:
+            # move image from upload directory to directory for matched face_id
+            print('Person for face ID {} is identified in {} with a confidence of {}.'.format(person.face_id, os.path.basename(image.name), person.candidates[0].confidence)) # Get topmost confidence score
+
+
+def extract_uploaded_image():
+    '''
+    Identify a face against a defined PersonGroup
+    '''
+    # Reference image for testing against
+    group_photo = 'test-image-person-group.jpg'
+    IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)))  #  change path for directory where just-uploaded pictures will be stored 
+    # Get test image
+    test_image_array = glob.glob(os.path.join(IMAGES_FOLDER, group_photo)) #  change as per above
+    image = open(test_image_array[0], 'r+b')
+    # Detect faces
+    face_ids = []
+    faces = face_client.face.detect_with_stream(image)
+    for face in faces:
+        face_ids.append(face.face_id)
+    identify_face(image, face_ids) # PEERSON_GROUP_ID
+
 
 def main():
     # Detect a face in an image that contains a single face
     # basically code to check if there is a face, if not  
     single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
     single_image_name = os.path.basename(single_face_image_url)
-    detected_faces = face_client.face.detect_with_url(url=single_face_image_url)  #  extracing name of file from url
+    detected_faces = face_client.face.detect_with_url(url=single_face_image_url)  #  extracting name of file from url
     if not detected_faces:
         raise Exception('No face detected from image {}'.format(single_image_name))
         #  add code to sort in 'objects/ no human' folder
@@ -44,3 +77,8 @@ def main():
 
 if "__name__" == "__main__":
     main()
+
+
+# for a new user
+# PERSON_GROUP_ID = 'my-unique-person-group'
+# face_client.person_group.create(person_group_id=PERSON_GROUP_ID, name=PERSON_GROUP_ID)
