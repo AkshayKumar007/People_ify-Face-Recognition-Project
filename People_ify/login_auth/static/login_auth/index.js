9bbc74c1d10a1fb7cd2db4299d39b65de54b0bba
AkshayKumar007 <akshaykrcc@live.com>
2019-10-30 17:28:05 +0530

    some improvements in frontend code

--- People_ify/login_auth/static/login_auth/index.js
+++ People_ify/login_auth/static/login_auth/index.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     var isopen = false;
-    document.querySelector('.openbtn').onclick = () => {
+
+    document.querySelector('.closebtn').onclick = () => {
         if(isopen==false){
-            document.getElementById("mySidebar").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
+            document.getElementById("mySidebar").style.width = "450px";
             isopen = true;
         }
-        
-    }
-      
-   document.querySelector('.closebtn').onclick = () => {
-        if(isopen==true){
+        else if(isopen==true){
             isopen = false;  
-            document.getElementById("mySidebar").style.width = "0px";
-            document.getElementById("main").style.marginLeft= "0px";
+            document.getElementById("mySidebar").style.width = "63px";            
         }
     }
+
+    // const a = document.querySelectorAll('.img1');
+    // a.style.animationPlayState() = 'paused';
+    window.onscroll = () => {
+        console.log('----');
+        console.log(window.innerHeight);
+        console.log(window.scrollY);
+        console.log(document.body.offsetHeight);
+        if (window.scrollY >= window.innerHeight) {
+            // a.style.animationDirection =  "reverse";
+            document.querySelector('body').style.background = 'green';
+
+        } else {
+            // a.style.animationDirection =  "normal";
+            document.querySelector('body').style.background = 'white';
+        }
+    };
+  
+  
 });
+
