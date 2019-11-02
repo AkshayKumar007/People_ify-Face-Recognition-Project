2e995b4fd3786b9b9ee904c79a0efe292f0253ac
AkshayKumar007 <akshaykrcc@live.com>
2019-11-02 16:18:48 +0530

    changes in login_auth views and added album_collection app

--- People_ify/login_auth/views.py
+++ People_ify/login_auth/views.py
@@ -1,44 +1,54 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render("People_ify/templates/index.html") #  index.html will be welcome screen
+    return render(request,"templates/login_auth/index.html")  # index.html will be welcome screen
 
-#  very Basic! Will add code to check is user already exists
+
 def register_view(request):
-    fname = request.POST["fname"]
-    lname = request.POST["lname"]
-    email = request.POST["email"]
-    uname = request.POST["uname"]
-    passwd = request.POST["passwd"] 
-    try:
-        u = User.objects.create_user(username=uname,first_name=fname, last_name=lname, email=email, password=passwd)
-        u.save()
-        return HttpResponseRedirect("homepage")  # check
-    except:
-        context = {
-            "message": "Something went wrong!"
-        }
-        return HttpResponseRedirect("register_view", context)
-    
+    if request.method == "GET":
+        return render(request,"templates/login_auth/register.html")
+
+    elif request.method == "POST":
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        email = request.POST["email"]
+        uname = request.POST["uname"]
+        passwd = request.POST["passwd"] 
+        res1 = User.objects.get(email=email)
+        res2 = User.objects.get(username=uname)
+        if res1 is not None:
+            return JsonResponse({"message": "email already registered! Please sign-in or use other email."}) 
+        elif res2 is not None:
+            return JsonResponse({"message": "username already registered! Please use other Username."})
+        else:
+            try:
+                u = User.objects.create_user(username=uname,first_name=fname, last_name=lname, email=email, password=passwd)
+                u.save()
+                # change for album_collection 
+                return HttpResponsePermanentRedirect("homepage")  # try HttpResponseRedirect
+            except:
+                return JsonResponse({"message": "Something went wrong!"})
+
 
 def login_view(request):
-    email = request.POST["email"]
-    passwd = request.POST["passwd"]
-    user = authenticate(request, email=email, password=passwd)
-    if user is not None:
-        login(request, user)
-        return redirect("homepage") #  1. have to create homepage  2. also check for HttpResponseRedirect
-    else:
-        context = { 
-            "message" : "Invalid email or password"
-        }
-        return redirect("login", context)
+    if request.method == "GET":
+        return render(request,"templates/login_auth/login.html")
+
+    elif request.method == "POST":
+        email = request.POST["email"]
+        passwd = request.POST["passwd"]
+        user = authenticate(request, email=email, password=passwd)
+        if user is not None:
+            login(request, user)
+            return redirect("homepage")  # 1. have to create homepage 2. also check for HttpResponseRedirect
+        else:
+            return JsonResponse({"message": "Incorrect email or Password"})
 
         
 @login_required(login_url="/login")
