b751198885db3e5fed18e805d78872c177c1969e
Kanishk Saxena <kanishksaxena5@gmail.com>
2019-11-15 01:07:50 +0530

    Update views.py
    tweaks need to be tested.

--- People_ify/album_collection/views.py
+++ People_ify/album_collection/views.py
@@ -15,13 +15,17 @@ def homepage(request, userid):
         # to be done by Kanishk
         # 1. get a "list" of all folder_names that are there inside picture/username excluding sample(be careful with paths)
         # 2. add that list to context below so that I can render it on webpage
-        folder_list = []
+        folder_list=[]
+
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path = os.path.join(BASE_DIR, userid)
+        folder_list =os.listdir(path) 
         context = {
             "uname": userid,
             "folders" : folder_list
 
         }
-        return render(request, "album_collection/collection.html", context)
+    return render(request, "album_collection/collection.html", context)
     
         
 # export DJANGO_SETTINGS_MODULE=People_ify.settings
@@ -38,4 +42,15 @@ def upload(request):
     # 1. everytime you create a pgp in face_identify, make it's entry in FolderName and all other tables
     # 2. see media upload documentation in django before proceeding
     # 3. call face_identify passing in PERSON_GROUP_ID = (requset.user.username).lower() as parameter
+    face_identify((request.user.username).lower())
+
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs=FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
     pass
